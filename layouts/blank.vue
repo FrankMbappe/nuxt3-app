c381aa9122368d3aa439086007613915e6965dae
Nicola Spadari <spadari.nicola@gmail.com>
2022-01-26 15:49:35 +0100

    Add 404 custom page

---
+++ layouts/blank.vue
@@ -0,0 +1,6 @@
+<template>
+	<div>
+		<StructureHeader />
+		<slot />
+	</div>
+</template>
---
+++ pages/[...all].vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="text-center pt-40">
+		<p class="md:text-5xl mb-5">
+			404
+		</p>
+
+		<NuxtLink to="/">
+			Go back
+		</NuxtLink>
+	</div>
+</template>
+
+<script lang="ts" setup>
+	definePageMeta({
+		layout: "blank"
+	});
+</script>
